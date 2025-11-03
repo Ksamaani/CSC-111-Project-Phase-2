62d09f3e2e06b24ea9f02bc1ac13f03a3ad3494f
ksamaani <Ksamaani@gmail.com>
2025-11-03 18:17:18 +0300

    Attributes is added

---
+++ src/Member.java
@@ -0,0 +1,17 @@
+public class Member {
+    //The Attributes
+    private int id;
+    private String name;
+    private int borrowedCount;
+    private int numViewBorrowed;
+    private int numBorrows;
+    private int numReturns;
+    private double sessionFees;
+
+    public double TotalRevenue;
+    public int TotalViewBorrowed;
+    public int TotalBorrows;
+    public int TotalReturns;
+
+
+}
