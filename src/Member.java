7f634d94e2523a9c4630011c10c78c8d28560f0b
ksamaani <Ksamaani@gmail.com>
2025-11-06 10:06:23 +0300

    canBorrow,canReturn methods is added

--- src/Member.java
+++ src/Member.java
@@ -19,6 +19,21 @@ public class Member {
         this.borrowedCount = borrowedCount;
     }
 
+    private boolean canBorrow(){
+        if (borrowedCount>=5){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    private boolean canReturn(){
+        if (borrowedCount <= 0){
+            return false;
+        }
+        else
+            return true;
+    }
 
 
 
