86afe7769403b28918b67d249e8db334f64455a8
ksamaani <Ksamaani@gmail.com>
2025-11-20 10:43:09 +0300

    more method added

--- src/Member.java
+++ src/Member.java
@@ -8,10 +8,10 @@ public class Member {
     private int numReturns;
     private double sessionFees;
 
-    public double TotalRevenue;
-    public int TotalViewBorrowed;
-    public int TotalBorrows;
-    public int TotalReturns;
+    public static double TotalRevenue;
+    public static int TotalViewBorrowed;
+    public static int TotalBorrows;
+    public static int TotalReturns;
 
     public Member (int id,String name,int borrowedCount){
         this.id = id;
@@ -35,6 +35,56 @@ public class Member {
             return true;
     }
 
+    public void viewBorrowedCount(){
+        numViewBorrowed++;
+        TotalViewBorrowed++;
+        System.out.println("The Number Of Borrowed Book: " + borrowedCount);
+    }
 
+    public boolean borrowOne (){
+        if (canBorrow()){
+            borrowedCount++;
+            numBorrows ++;
+            sessionFees += 0.5;
+            TotalRevenue += 0.5;
+            TotalBorrows ++;
+            System.out.printf("You have borrowed Book!");
+            return true;
+        }
+        else {
+            System.out.printf("You have 5 books borrowed %nYou Can not borrow more Books!");
+            return false;
+        }
+
+    }
+
+    public boolean returnOne(){
+        if (canReturn()){
+            borrowedCount--;
+            numReturns++;
+            TotalReturns++;
+            System.out.printf("%nYou have Returned Book!");
+            return true;
+        }
+        else{
+            System.out.printf("%n You have No Books to Return");
+            return false;
+        }
+
+    }
+
+    public void displayStatistics(){
+        System.out.println("the number of times the operation View borrowed books count was use :"+numViewBorrowed);
+        System.out.println("the number of times the operation Borrow book was used : " + numBorrows);
+        System.out.println("the number of times the operation Return book was used: " + numReturns);
+        System.out.println("the total amount of fees that incurred during the session is : "+ sessionFees);
+    }
+
+    public void reset() {
+        numViewBorrowed =0;
+        numBorrows = 0;
+        numReturns = 0;
+        sessionFees = 0;
+    }
 
 }
